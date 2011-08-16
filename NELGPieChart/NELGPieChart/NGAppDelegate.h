759e14dc643df82737418f1b9ebe68d7b68a10b4
Murali Raghuram <murali.raghuram@gmail.com>
2011-08-15 19:51:22 -0500

    first commit

---
+++ NELGPieChart/NELGPieChart/NGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NGAppDelegate.h
+//  NELGPieChart
+//
+//  Created by Murali Raghuram on 8/15/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NELGPieChart/NELGPieChart/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NELGPieChart
+//
+//  Created by Murali Raghuram on 8/15/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([NGAppDelegate class]));
+	}
+}
